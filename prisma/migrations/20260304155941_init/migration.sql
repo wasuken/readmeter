60264527459061d18a04d48f0acf90ce251335f1
wasu <wasuken1@gmail.com>
2026-03-05 01:19:12 +0900

    Prisma周り対応

---
+++ prisma/migrations/20260304155941_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "isbn" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "rating" INTEGER
+);
